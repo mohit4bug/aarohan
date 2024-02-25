fca480bc680c6ee808d8abad8c3ae706c5705fe2
Mohit Khatri <mohit4bug@gmail.com>
2024-02-25 16:59:13 +0530

    feat: added links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,17 +3,33 @@
 import { LogoutAlertDialog } from "@/components/logout-alert-dialog"
 import { Button } from "@/components/ui/button"
 import { LogOutIcon } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export const Navbar = () => {
+  const router = useRouter()
+  const handleClick = (href: string) => router.push(href)
+
   return (
-    <nav className="min-h-[4rem] max-h-[4rem] border-b px-4 xl:px-0">
+    <header className="min-h-[4rem] max-h-[4rem] border-b px-4 xl:px-0">
       <div className="h-full max-w-6xl mx-auto items-center flex justify-between">
+        <nav className="ml-auto gap-x-8 flex">
+          <p
+            className="text-sm font-medium hover:underline cursor-pointer"
+            onClick={handleClick.bind(null, "/events")}>
+            Events
+          </p>
+          <p
+            className="text-sm font-medium hover:underline cursor-pointer"
+            onClick={handleClick.bind(null, "/dashboard")}>
+            Dashboard
+          </p>
+        </nav>
         <LogoutAlertDialog>
-          <Button variant="outline" className="ml-auto">
+          <Button variant="outline" className="ml-8">
             <LogOutIcon className="w-4 h-4 mr-2" /> Sign out
           </Button>
         </LogoutAlertDialog>
       </div>
-    </nav>
+    </header>
   )
 }
