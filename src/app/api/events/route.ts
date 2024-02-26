25c235a1832a5068e296ca284b83932f1de86a2c
Mohit Khatri <mohit4bug@gmail.com>
2024-02-26 06:36:58 +0530

    fix: added one more fix revalidatePath(request.url)

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,9 +1,11 @@
 import { db } from "@/lib/prisma"
+import { revalidatePath } from "next/cache"
 
 export async function GET(request: Request) {
   try {
     const events = await db.event.findMany()
 
+    revalidatePath(request.url)
     return Response.json(
       {
         events,
